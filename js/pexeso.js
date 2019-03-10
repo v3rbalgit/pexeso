28a98c85eaa3176c62e1d7c51abc07adda34be15
Bryan Veer <soundspeed@protonmail.com>
2019-03-11 00:55:22 +0100

    Refactor to use Unsplash API for images

--- js/pexeso.js
+++ js/pexeso.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const pexeso = document.getElementById('pexeso');
 const grid = 4;
 const tiles = [];
@@ -8,7 +16,7 @@ const tiles = [];
 // Gridtile class
 class Tile {
     constructor(tile) {
-        // image
+        // image url
         this.imageUrl = '';
         this.element = document.createElement('div');
         this.element.className = 'tile';
@@ -19,7 +27,28 @@ class Tile {
         if (pexeso)
             pexeso.appendChild(this.element);
     }
-    /* Generate an image for the tile */
+    // !!! initialize the imagebase before making the grid
+    static init(keyword) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = 'https://api.unsplash.com/photos/random?query=supercar';
+            if (keyword)
+                url = `https://api.unsplash.com/photos/random?query=${keyword}`;
+            for (let i = 0; i < 8; i++) {
+                let image = yield fetch(url, {
+                    method: 'GET',
+                    mode: 'cors',
+                    cache: 'no-cache',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Accept-Version': 'v1',
+                        Authorization: 'Client-ID f3cc7453479d252ec931a8004639aae4449b677cc54af88c7553aab80d2e604f'
+                    }
+                }).then(response => response.json());
+                Tile.imageBase.push(image.urls.small);
+            }
+        });
+    }
+    /* Generate an image for tile */
     generateImage() {
         let image = document.createElement('div');
         image.className = 'image';
@@ -29,31 +58,40 @@ class Tile {
             image.style.backgroundImage = `url(${random})`;
         return image;
     }
-    /* Generate event listener */
+    /* Game logic */
     addListener(element) {
         element.addEventListener('click', listener);
         const self = this;
+        // event callback function
         function listener(e) {
             e.target.style.opacity = '1';
             let parent = e.target.parentElement;
+            // parent null check
             if (parent) {
                 if (Tile.challenge.length === 0) {
                     Tile.challenge.push([e.target, parent.id, self.imageUrl]);
                     Tile.uncovered.push(parent.id);
                 }
                 else {
+                    // the previously clicked tile cannot be clicked on again
                     if (Tile.challenge[0][1] !== parent.id) {
+                        // check for match to the previously click tile
                         if (self.imageUrl === Tile.challenge[0][2]) {
+                            // make sure matching tiles are not clickable anymore
                             parent.removeEventListener('click', listener);
                             let parent2 = Tile.challenge[0][0].parentElement;
                             if (parent2) {
                                 parent2.removeEventListener('click', listener);
+                                // increment matched tiles
                                 Tile.uncovered.push(parent2.id);
                             }
+                            // reset
                             Tile.challenge = [];
+                            // game over when all used images are matched
                             if (Tile.uncovered.length === Tile.imagesUsed.length) {
                                 console.log('Game over');
                             }
+                            // hide both uncovered images and reset if previously clicked tile doesn't match current tile
                         }
                         else {
                             setTimeout(() => {
@@ -70,8 +108,8 @@ class Tile {
     }
     // Recursive helper function
     randomImage() {
-        let random = Math.floor(Math.random() * 8) + 1;
-        let imagePath = `img/cars${random}.jpg`;
+        let random = Math.floor(Math.random() * 8);
+        let imagePath = Tile.imageBase[random];
         let filteredArray = Tile.imagesUsed.filter(el => el === imagePath);
         if (filteredArray.length < 2) {
             Tile.imagesUsed.push(imagePath);
@@ -82,14 +120,18 @@ class Tile {
         }
     }
 }
-// array of all images in the grid
+Tile.imageBase = [];
+// array of all images used in the grid
 Tile.imagesUsed = [];
-// store previously click image
+// store previously clicked image element, parent id, and image url
 Tile.challenge = [];
-// store parent ids of images element
+// store parent ids of image elements
 Tile.uncovered = [];
-// Generate gridtiles
-for (let i = 1; i <= Math.pow(grid, 2); i++) {
-    let tile = new Tile(i);
-    tiles[i - 1] = tile;
-}
+Tile.init().then(() => {
+    // Generate gridtiles
+    for (let i = 1; i <= Math.pow(grid, 2); i++) {
+        let tile = new Tile(i);
+        tiles[i - 1] = tile;
+    }
+    console.log(tiles);
+}).catch(err => console.error(err));
--- js/pexeso.ts
+++ js/pexeso.ts
@@ -10,11 +10,11 @@ const tiles: Array<Tile> = [];
 
 // Gridtile class
 class Tile {
-  // div containing the image element
+  // div with id containing the image element
   element: HTMLDivElement;
   // image element
   image: HTMLDivElement;
-  // image
+  // image url
   imageUrl = '';
 
   constructor(tile: number) {
@@ -27,16 +27,38 @@ class Tile {
     if (pexeso) pexeso.appendChild(this.element);
   }
 
-  // array of all images in the grid
+  private static imageBase: Array<string> = [];
+
+  // array of all images used in the grid
   private static imagesUsed: Array<string> = [];
 
-  // store previously click image
+  // store previously clicked image element, parent id, and image url
   private static challenge: Array<[HTMLDivElement, string, string]> = [];
 
-  // store parent ids of images element
+  // store parent ids of image elements
   private static uncovered: Array<string> = [];
 
-  /* Generate an image for the tile */
+  // !!! initialize the imagebase before making the grid
+  static async init(keyword?: string): Promise<void> {
+    let url = 'https://api.unsplash.com/photos/random?query=supercar';
+    if (keyword) url = `https://api.unsplash.com/photos/random?query=${keyword}`;
+
+    for (let i = 0; i < 8; i++) {
+      let image = await fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Accept-Version': 'v1',
+          Authorization: 'Client-ID f3cc7453479d252ec931a8004639aae4449b677cc54af88c7553aab80d2e604f'
+        }
+      }).then(response => response.json());
+      Tile.imageBase.push(image.urls.small);
+    }
+  }
+
+  /* Generate an image for tile */
   private generateImage(): HTMLDivElement {
     let image: HTMLDivElement = document.createElement('div');
     image.className = 'image';
@@ -46,32 +68,42 @@ class Tile {
     return image;
   }
 
-  /* Generate event listener */
+  /* Game logic */
   private addListener(element: HTMLDivElement): void {
     element.addEventListener('click', listener);
     const self = this;
 
+    // event callback function
     function listener(e: Event): void {
       (<HTMLDivElement>e.target).style.opacity = '1';
       let parent = (<HTMLDivElement>e.target).parentElement;
 
+      // parent null check
       if (parent) {
         if (Tile.challenge.length === 0) {
           Tile.challenge.push([<HTMLDivElement>e.target, parent.id, self.imageUrl]);
           Tile.uncovered.push(parent.id);
         } else {
+          // the previously clicked tile cannot be clicked on again
           if (Tile.challenge[0][1] !== parent.id) {
+            // check for match to the previously click tile
             if (self.imageUrl === Tile.challenge[0][2]) {
+              // make sure matching tiles are not clickable anymore
               parent.removeEventListener('click', listener);
+
               let parent2 = Tile.challenge[0][0].parentElement;
               if (parent2) {
                 parent2.removeEventListener('click', listener);
+                // increment matched tiles
                 Tile.uncovered.push(parent2.id);
               }
+              // reset
               Tile.challenge = [];
+              // game over when all used images are matched
               if (Tile.uncovered.length === Tile.imagesUsed.length) {
                 console.log('Game over');
               }
+              // hide both uncovered images and reset if previously clicked tile doesn't match current tile
             } else {
               setTimeout(() => {
                 (<HTMLDivElement>e.target).style.opacity = '0';
@@ -88,9 +120,10 @@ class Tile {
 
   // Recursive helper function
   private randomImage(): string {
-    let random = Math.floor(Math.random() * 8) + 1;
-    let imagePath = `img/cars${random}.jpg`;
+    let random = Math.floor(Math.random() * 8);
+    let imagePath = Tile.imageBase[random];
     let filteredArray = Tile.imagesUsed.filter(el => el === imagePath);
+
     if (filteredArray.length < 2) {
       Tile.imagesUsed.push(imagePath);
       return imagePath;
@@ -100,8 +133,11 @@ class Tile {
   }
 }
 
-// Generate gridtiles
-for (let i = 1; i <= grid ** 2; i++) {
-  let tile: Tile = new Tile(i);
-  tiles[i - 1] = tile;
-}
+Tile.init().then(() => {
+  // Generate gridtiles
+  for (let i = 1; i <= grid ** 2; i++) {
+    let tile: Tile = new Tile(i);
+    tiles[i - 1] = tile;
+  }
+  console.log(tiles);
+}).catch(err => console.error(err));
