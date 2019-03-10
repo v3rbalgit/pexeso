e85bf59bc67ea793d25be3bf3a01544331bf20ee
Bryan Veer <soundspeed@protonmail.com>
2019-03-10 22:28:58 +0100

    Event listeners, photo swap, adjust tsconfig

--- js/pexeso.js
+++ js/pexeso.js
@@ -2,25 +2,80 @@
 const pexeso = document.getElementById('pexeso');
 const grid = 4;
 const tiles = [];
+// interface ImageEventTarget {
+//   target: HTMLDivElement;
+// }
+// Gridtile class
 class Tile {
     constructor(tile) {
+        // image
+        this.imageUrl = '';
         this.element = document.createElement('div');
         this.element.className = 'tile';
         this.element.id = `tile${tile}`;
         this.image = this.generateImage();
         this.element.appendChild(this.image);
+        this.addListener(this.element);
         if (pexeso)
             pexeso.appendChild(this.element);
     }
+    /* Generate an image for the tile */
     generateImage() {
-        let image = document.createElement('img');
+        let image = document.createElement('div');
+        image.className = 'image';
         let random = randomImage();
+        this.imageUrl = random;
         if (random)
-            image.setAttribute('src', random);
+            image.style.backgroundImage = `url(${random})`;
         return image;
     }
+    /* Generate event listener */
+    addListener(element) {
+        element.addEventListener('click', listener);
+        const self = this;
+        function listener(e) {
+            e.target.style.opacity = '1';
+            let parent = e.target.parentElement;
+            if (parent) {
+                if (Tile.challenge.length === 0) {
+                    Tile.challenge.push([e.target, parent.id, self.imageUrl]);
+                    Tile.uncovered.push(parent.id);
+                }
+                else {
+                    if (Tile.challenge[0][1] !== parent.id) {
+                        if (self.imageUrl === Tile.challenge[0][2]) {
+                            parent.removeEventListener('click', listener);
+                            let parent2 = Tile.challenge[0][0].parentElement;
+                            if (parent2) {
+                                parent2.removeEventListener('click', listener);
+                                Tile.uncovered.push(parent2.id);
+                            }
+                            Tile.challenge = [];
+                            if (Tile.uncovered.length === Tile.imagesUsed.length) {
+                                console.log('Game over');
+                            }
+                        }
+                        else {
+                            setTimeout(() => {
+                                e.target.style.opacity = '0';
+                                Tile.challenge[0][0].style.opacity = '0';
+                                Tile.challenge = [];
+                                Tile.uncovered.pop();
+                            }, 1000);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+// array of all images in the grid
 Tile.imagesUsed = [];
+// store previously click image
+Tile.challenge = [];
+// store parent ids of images element
+Tile.uncovered = [];
+// Recursive helper function
 function randomImage() {
     let random = Math.floor(Math.random() * 8) + 1;
     let imagePath = `img/cars${random}.jpg`;
@@ -33,7 +88,8 @@ function randomImage() {
         return randomImage();
     }
 }
+// Generate gridtiles
 for (let i = 1; i <= Math.pow(grid, 2); i++) {
-    let element = new Tile(i);
-    tiles[i] = element;
+    let tile = new Tile(i);
+    tiles[i - 1] = tile;
 }
--- js/pexeso.ts
+++ js/pexeso.ts
@@ -4,9 +4,18 @@ const grid: number = 4;
 
 const tiles: Array<Tile> = [];
 
+// interface ImageEventTarget {
+//   target: HTMLDivElement;
+// }
+
+// Gridtile class
 class Tile {
+  // div containing the image element
   element: HTMLDivElement;
-  image: HTMLElement;
+  // image element
+  image: HTMLDivElement;
+  // image
+  imageUrl = '';
 
   constructor(tile: number) {
     this.element = document.createElement('div');
@@ -14,19 +23,71 @@ class Tile {
     this.element.id = `tile${tile}`;
     this.image = this.generateImage();
     this.element.appendChild(this.image);
+    this.addListener(this.element);
     if (pexeso) pexeso.appendChild(this.element);
   }
 
+  // array of all images in the grid
   static imagesUsed: Array<string> = [];
 
-  protected generateImage(): HTMLElement {
-    let image: HTMLImageElement = document.createElement('img');
-    let random = randomImage();
-    if (random) image.setAttribute('src', random);
+  // store previously click image
+  static challenge: Array<[HTMLDivElement, string, string]> = [];
+
+  // store parent ids of images element
+  static uncovered: Array<string> = [];
+
+  /* Generate an image for the tile */
+  generateImage(): HTMLDivElement {
+    let image: HTMLDivElement = document.createElement('div');
+    image.className = 'image';
+    let random: string = randomImage();
+    this.imageUrl = random;
+    if (random) image.style.backgroundImage = `url(${random})`;
     return image;
+  }
+
+  /* Generate event listener */
+  addListener(element: HTMLDivElement): void {
+    element.addEventListener('click', listener);
+    const self = this;
+
+    function listener(e: Event): void {
+      (<HTMLDivElement>e.target).style.opacity = '1';
+      let parent = (<HTMLDivElement>e.target).parentElement;
+
+      if (parent) {
+        if (Tile.challenge.length === 0) {
+          Tile.challenge.push([(<HTMLDivElement>e.target), parent.id, self.imageUrl]);
+          Tile.uncovered.push(parent.id);
+        } else {
+          if (Tile.challenge[0][1] !== parent.id) {
+            if (self.imageUrl === Tile.challenge[0][2]) {
+              parent.removeEventListener('click', listener);
+              let parent2 = Tile.challenge[0][0].parentElement;
+              if (parent2) {
+                parent2.removeEventListener('click', listener);
+                Tile.uncovered.push(parent2.id);
+              }
+              Tile.challenge = [];
+              if (Tile.uncovered.length === Tile.imagesUsed.length) {
+                console.log('Game over');
+              }
+            } else {
+              setTimeout(() => {
+                (<HTMLDivElement>e.target).style.opacity = '0';
+                Tile.challenge[0][0].style.opacity = '0';
+                Tile.challenge = [];
+                Tile.uncovered.pop();
+              }, 1000);
+            }
+          }
+        }
+      }
+    }
   }
 }
 
+// Recursive helper function
 function randomImage(): string {
   let random = Math.floor(Math.random() * 8) + 1;
   let imagePath = `img/cars${random}.jpg`;
@@ -39,7 +100,8 @@ function randomImage(): string {
   }
 }
 
-for (let i = 1; i <= grid**2; i++) {
-  let element: Tile = new Tile(i);
-  tiles[i] = element;
+// Generate gridtiles
+for (let i = 1; i <= grid ** 2; i++) {
+  let tile: Tile = new Tile(i);
+  tiles[i - 1] = tile;
 }
